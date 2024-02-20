94f16df4a4626bcdcb93281016bf5cd2ffb80a6d
Sanel Ryan <sanelryantclitus@gmail.com>
2024-02-21 03:13:08 +0530

    port dynamic

--- index.js
+++ index.js
@@ -160,9 +160,8 @@ app.get("/unsubscribe", async (req, res) => {
 	}
 });
 
-const port = process.env.PORT;
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
 
 function validateEmail(email) {
